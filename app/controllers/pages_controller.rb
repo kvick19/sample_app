374e825f80513f5aab1581582549d6560d22adf2
Krista Vick <kristalvick@gmail.com>
2012-03-03 01:42:15 -0500

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
-  def home2
+  def home
+    @title = "Home"
+  end
+  
+  def contact
+     @title = "Contact"
+  end
+  
+  def about
+     @title = "About"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "pages/home2"
+  get "pages/about"
 
   get "pages/home"
 
