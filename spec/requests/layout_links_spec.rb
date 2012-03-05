f5b0f9aee3a4a1097e25f893737bb5789ab7b14a
Krista Vick <kristalvick@gmail.com>
2012-03-05 02:24:33 -0500

    Added a Users controller and layout controllers

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
  
 describe "LayoutLinks" do
-it  "should have a Home page at '/'" do
-  get'/'
-  response.should have_selector('title', :key => "Home")
+it "should have a Home page at '/'" do
+  get '/'
+  response.should have_selector('title', :content => "Home")
 end
 
 it "should have a contact page at '/contact'" do
@@ -21,5 +21,35 @@ it "should have an Help page at '/help'" do
   get '/help'
   response.should have_selector('title', :content =>"Help")
 end
+
+it "should have an signup page at '/signup'" do
+  get '/signup'
+  response.should have_selector('title', :content =>"Sign up")
 end
+
+it "should have the right links on the layout" do
+  visit root_path
+  response.should have_selector('title', :content => "Home")
+  click_link "About"
+  response.should have_selector('title', :content => "About")
+  click_link "Contact"
+  response.should have_selector('title', :content => "Contact")
+  click_link "Home"
+  response.should have_selector('title', :content => "Home")
+  click_link "Sign up now!"
+  response.should have_selector('title', :content => "Sign up")
+  response.should have_selector('a[href="/"]>img')
+end
+
+end
+
+
+
+
+
+
+
+
+
+
 
