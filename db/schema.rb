cfbe3cc28a54be86005f2c2584018f2ef909d8fa
Krista Vick <kristalvick@gmail.com>
2012-03-06 17:05:06 -0500

    Added password and encrypted_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120306013833) do
+ActiveRecord::Schema.define(:version => 20120306215529) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
