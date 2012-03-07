496badd10b05bf4d083b36db16e75de92e6efbfe
Krista Vick <kristalvick@gmail.com>
2012-03-06 22:30:19 -0500

    Added a User factory and tests for the show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,15 +2,33 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+  
+  before(:each) do
+    @user = Factory(:user)
+  end
+  
+  
+  describe "Get 'show'" do
+    it "should be successful" do
+      get :show, :id =>@user
+      response.should be_success
+    end
+    
+    it "should find the right user" do
+      get :show, :id=>@user
+      assigns(:user).should == @user
+    end
+    
+  end
 
   describe "GET 'new'" do
     it "should be successfull" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector('title', :content => "Sign up")
     end
     
